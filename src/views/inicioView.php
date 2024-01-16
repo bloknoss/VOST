b9c88e4d41d3821f3abb07b947729d44e5be957c
gsanlop705 <gsanlop705@g.educaand.es>
2024-01-16 18:10:44 +0100

    view config

--- src/views/inicioView.php
+++ src/views/inicioView.php
@@ -7,8 +7,45 @@
     <title>Document</title>
 </head>
 <header>
+        <div class="header__superior">
+            <div class="logo">
+                <img src="img/logo-magtimus-small-v2.png" alt="">
+            </div>
+            <div class="search">
+                <input type="search" placeholder="¿Qué deseas buscar?">
+            </div>
+        </div>
 
-</header>
+        <div class="container__menu">
+
+            <div class="menu">
+                <input type="checkbox" id="check__menu">
+                <label for="check__menu" id="label__check">
+                    <i class="fas fa-bars icon__menu"></i>
+                </label>
+                <nav>
+                    <ul>
+                        <li><a href="#" id="selected"></a></li>
+                        <li><a href="#">Servicios</a>
+                            <ul>
+                                <li><a href="#">Diseño Web</a></li>
+                                <li><a href="#">Diseño Gráfico</a></li>
+                                <li><a href="#">Ordenadores</a></li>
+                                <li><a href="#">Servidores</a></li>
+                                <li><a href="#">Analista</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Cursos</a></li>
+                        <li><a href="#">Contactos</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+
+    </header>
 
 <body>
 
