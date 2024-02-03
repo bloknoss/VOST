9f6b67cae61235af9a47f1d6d135e223c333fe49
bloknoss <windpourwow@gmail.com>
2024-02-03 13:40:18 +0100

    iniciado carrito y fixeado vinyl (faltaba nombre, soy imbecil)

---
+++ src/models/CartModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace VOST\models;
+
+use PDOException;
+
+include_once 'Database.php';
+
+class CartModel
+{
+
+    public static function getCartItems($pdo, $cartId) : array | null
+    {
+        $reqResults = Database::getItems($pdo, "carts");
+        //TODO: Crear la clase abstracta de el carrito, una vez hecho devolver la clase construida.
+        return $reqResults;
+    }
+
+    // TODO: Falta tener información de la base de datos.
+    public static function addToCart($pdo, $vinyl)
+    {
+        try {
+
+        } catch (PDOException $e) {
+
+        } finally {
+        }
+    }
+
+    public static function deleteFromCart($pdo, $vinyl)
+    {
+
+        try {
+
+        } catch (PDOException $e) {
+
+        } finally {
+        }
+    }
+
+    public static function deleteCart($pdo, $cartId)
+    {
+        // TODO: Aquí falta la clase del carrito, todo está a medias.
+        Database::deleteItem($pdo, $cartId);
+    }
+
+}
