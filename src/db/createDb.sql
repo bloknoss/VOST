06bbf4bed06e9b081fd234aca023bd6b6fa0ba58
bloknoss <windpourwow@gmail.com>
2024-02-03 19:07:19 +0100

    new fix, missing quantity on db

--- src/db/createDb.sql
+++ src/db/createDb.sql
@@ -84,6 +84,7 @@ drop table if exists carts_vinyls;
 create table carts_vinyls (
     id_user int not null,
     id_vinyl int not null,
+    quantity int not null,
     constraint foreign key (id_user) references users (id_user),
     constraint foreign key (id_vinyl) references vinyls (id_vinyl),
     primary key (id_user, id_vinyl)
