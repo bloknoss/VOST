1a90bd72e5bd703f3e68f32cef53939089f20b08
gsanlop705 <gsanlop705@g.educaand.es>
2024-02-05 23:53:47 +0100

    s

--- src/views/tienda.php
+++ src/views/tienda.php
@@ -1,93 +1,268 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>VOST</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+    />
     <link rel="stylesheet" href="/css/tienda.css" />
     <link rel="shortcut icon" href="/public/assets/images/logo.png" type="image/x-icon" />
-</head>
-<header class="header">
+  </head>
+  <header class="header">
     <div class="logo">
-        <!---->
-        <img src="/public/assets/images/logo.png" alt="Logo de la marca" />
+      <!---->
+      <img src="img/logo.png" alt="Logo de la marca" />
     </div>
     <nav>
-        <ul class="nav-links">
-            <li><a href="#">Generos</a></li>
-            <li><a href="#">hhhhhhh</a></li>
-            <li><a href="#">hhhhhhh</a></li>
-        </ul>
+      <ul class="nav-links">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Tienda</a></li>
+        <li><a href="#">Carrito</a></li>
+      </ul>
     </nav>
     <a class="btn" href="#"><button>Inicia Sesión / Registrate</button></a>
 
     <a onclick="openNav()" class="menu" href="#"><button>Menu</button></a>
 
     <div id="mobile-menu" class="overlay">
-        <a onclick="closeNav()" href="" class="close">&times;</a>
+      <a onclick="closeNav()" href="" class="close">&times;</a>
 
-        <div class="overlay-content">
-            <a href="#">Generos</a>
+      <div class="overlay-content">
+        <a href="#">Inicio</a>
 
-            <a href="#">hhhhhhh</a>
+        <a href="#">Tienda</a>
 
-            <a href="#">hhhhhhh</a>
+        <a href="#">Carrito</a>
 
-            <a href="#">Contacto</a>
-        </div>
+        <a href="#">Cuenta</a>
+      </div>
     </div>
-</header>
+  </header>
 
-<body>
+  <body>
+    <div class="container">
+      <div class="lineas"></div>
+      <h1>TIENDA</h1>
+      <div class="lineas"></div>
+    </div>
+
     <div class="productos">
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
-        <img src="/public/assets/images/sfdk.png" alt="" />
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="carta">
+        <div class="card-img"><img src="/public/assets/images/sfdk.png" alt="" /></div>
+        <div class="info-card">
+          <div class="text-product">
+            <h3>Inkebrantable</h3>
+            <p class="category">SFDK</p>
+          </div>
+          <div class="price"><h3>30€</h3></div>
+        </div>
+        <div class="comprar">
+          <button>Ver más</button>
+        </div>
+      </div>
     </div>
-</body>
-<footer>
+  </body>
+  <footer>
     <div class="contenedor-footer">
-        <div class="content-foo">
-            <h4>Atención al cliente</h4>
-            <ul>
-                <li><a href="#">Q&A</a></li>
-                <li><a href="#">Envíos y pagos</a></li>
-                <li><a href="#">Cambios y devoluciones</a></li>
-                <li><a href="#">vostMusic@gmail.com</a></li>
-            </ul>
+      <div class="content-foo">
+        <h4>Atención al cliente</h4>
+        <ul>
+          <li><a href="#">Q&A</a></li>
+          <li><a href="#">Envíos y pagos</a></li>
+          <li><a href="#">Cambios y devoluciones</a></li>
+          <li><a href="#">vostMusic@gmail.com</a></li>
+        </ul>
+      </div>
+      <div class="content-foo">
+        <h4>Empresa</h4>
+        <ul>
+          <li><a href="#">Sobre nosotros</a></li>
+          <li><a href="#">Política de privacidad</a></li>
+          <li><a href="#">Política de cookies</a></li>
+          <li><a href="#">Condiciones generales</a></li>
+        </ul>
+      </div>
+      <div class="content-foo">
+        <h4>Redes sociales</h4>
+        <div class="social-links">
+          <a href="#"
+            ><img
+              src="/public/assets/images/fb.png"
+              alt="Logo de la marca"
+              height="40px"
+          /></a>
+          <a href="#"
+            ><img
+              src="/public/assets/images/tw.png"
+              alt="Logo de la marca"
+              height="40px"
+          /></a>
+          <a href="#"
+            ><img
+              src="/public/assets/images/ig.png"
+              alt="Logo de la marca"
+              height="40px"
+          /></a>
         </div>
-        <div class="content-foo">
-            <h4>Empresa</h4>
-            <ul>
-                <li><a href="#">Sobre nosotros</a></li>
-                <li><a href="#">Política de privacidad</a></li>
-                <li><a href="#">Política de cookies</a></li>
-                <li><a href="#">Condiciones generales</a></li>
-            </ul>
-        </div>
-        <div class="content-foo">
-            <h4>Redes sociales</h4>
-            <div class="social-links">
-                <a href="#"><img src="/public/assets/images/fb.png" alt="Logo de la marca" height="40px" /></a>
-                <a href="#"><img src="/public/assets/images/tw.png" alt="Logo de la marca" height="40px" /></a>
-                <a href="#"><img src="/public/assets/images/ig.png" alt="Logo de la marca" height="40px" /></a>
-            </div>
-        </div>
+      </div>
     </div>
     <div class="content-fooo">
-        <h2 class="titulo-final">&copy; VOST</h2>
-        <img src="/public/assets/images/metpago.png" alt="metpago" height="40px" />
+      <h2 class="titulo-final">&copy; VOST</h2>
+      <img
+        src="/public/assets/images/metpago.png"
+        alt="metpago"
+        height="40px"
+      />
     </div>
-</footer>
+  </footer>
 
-<!---->
-<script type="text/javascript" src="nav.js"></script>
-
+  <!---->
+  <script type="text/javascript" src="nav.js"></script>
 </html>
